a070c74443b2f456e56e358f46a1bd87ab9e46db
小野寺勇磨 <yuma.onodera0913@gmail.com>
2022-09-03 15:52:41 +0900

    メールアドレス変更の際に、メールアドレス確認のメールを送信するよう修正

--- app/UseCases/User/UpdateEmailAction.php
+++ app/UseCases/User/UpdateEmailAction.php
@@ -3,7 +3,6 @@
 namespace App\UseCases\User;
 
 use App\Http\Requests\User\UpdateEmailRequest;
-use App\Models\User;
 
 class UpdateEmailAction
 {
@@ -13,11 +12,13 @@ class UpdateEmailAction
      */
     public function __invoke(UpdateEmailRequest $request): void
     {
-        $user = User::where('id', auth()->id());
+        $user = $request->user();
 
         $user->update([
             'email' => $request->email,
             'email_verified_at' => null
         ]);
+
+        $user->sendEmailVerificationNotification();
     }
 }
