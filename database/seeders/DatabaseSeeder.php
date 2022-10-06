04bb7f34a4da6bcfc54fc7a80280dcc46abd0cba
小野寺勇磨 <yuma.onodera0913@gmail.com>
2022-10-06 13:40:46 +0900

    シーダー実行時にデータを検索インデックスに反映させる

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
@@ -23,5 +26,29 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Schema::enableForeignKeyConstraints();
+
+        $models = [
+            User::class,
+            Post::class
+        ];
+        $this->ScoutImport($models);
+    }
+
+    /**
+     * データを検索インデックスに反映させる
+     *
+     * @param array $models
+     * @return void
+     */
+    private function ScoutImport(array $models): void
+    {
+        foreach ($models as $model) {
+            Artisan::call('scout:flush', [
+                'model' => $model
+            ]);
+            Artisan::call('scout:import', [
+                'model' => $model
+            ]);
+        }
     }
 }
