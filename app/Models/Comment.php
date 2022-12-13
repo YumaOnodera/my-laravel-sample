5e0a222347666698f45890af5f921d97fdf4d36a
小野寺勇磨 <yuma.onodera0913@gmail.com>
2022-12-13 13:34:51 +0900

    リレーションの調整

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -38,14 +38,6 @@ class Comment extends Model
     /**
      * @return BelongsTo
      */
-    public function post(): BelongsTo
-    {
-        return $this->belongsTo(Post::class);
-    }
-
-    /**
-     * @return BelongsTo
-     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Scout\Searchable;
 
 class Post extends Model
@@ -39,6 +40,14 @@ class Post extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 
     /**
